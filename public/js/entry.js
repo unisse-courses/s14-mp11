206b9079994b768416ea7c1c9bfeff4c8b38e294
AnjeloTing <anjelo1199@yahoo.com>
2020-03-22 10:31:43 +0800

    Entry Js added
    entry validation

---
+++ public/js/entry.js
@@ -0,0 +1,44 @@
+function checkLogin()
+{
+    console.log("Checking form");
+    var username = document.forms["login-form"]["loginUsername"].value;
+    var password = document.forms["login-form"]["loginPassword"].value;
+    if (username == null || username == "", password == null || password == "") {
+        alert("Login details not filled")
+        return false;
+    }
+    // TODO: Insert function where it will find and match to the database
+    window.location.href = "../../profile";   
+    return true;
+}
+
+function checkRegister()
+{
+    console.log("Checking form");
+    var email = document.forms["registration-form"]["registerEmail"].value;
+    var name = document.forms["registration-form"]["registerName"].value;
+    // var username = document.forms["registration-form"]["registerImage"].value;
+    var username = document.forms["registration-form"]["registerUsername"].value;
+    var pass1 = document.forms["registration-form"]["registerPassword1"].value;
+    var pass2 = document.forms["registration-form"]["registerPassword2"].value;
+    var description = document.forms["registration-form"]["registerDescription"].value;
+    var checkbox = document.getElementById("registerCheckbox");
+    if (username == null || username == "",email == null || email == "",name == null || name == "",
+        pass1 == null || pass1 == "",pass2 == null || pass2 == "",description == null || description == "") {
+            
+            alert("Fill in the registration form & check the agreement to terms and conditions");
+            return false;
+        }
+    if(checkbox.checked==false)
+    {
+        alert("Please check the terms and conditions");
+        return false;
+    }
+    if(pass1!=pass2)
+    {
+        alert("Password does not match");
+        return false;
+    }
+    window.location.href = "../../profile";     
+    return true;
+}
